94c2aacdda0bd9e5d0be7c8190fac3557bedaadc
AliMoha99 <Alimelsayed6@gmail.com>
2022-02-22 18:17:01 +0200

    second commit

---
+++ pro.js
@@ -0,0 +1,10 @@
+// var menue =document.getElementsByClassName("toggle-menu")[0];
+// menue.addEventListener("click",function(){
+//     console.log(1);
+//     var x = document.getElementsByClassName("links")[0];
+//     if (x.style.display === 'none') {
+//       x.style.display = 'block';
+//     } else {
+//       x.style.display = 'none';
+//     }
+// });
